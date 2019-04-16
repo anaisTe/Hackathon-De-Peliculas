713f4e90ad024654e3f10532d30bf6c931553a5e
ivanaolortegui <olorteguimor@gmail.com>
2019-04-16 11:12:22 -0500

    Agregando primera historia de usuario

--- src/main.js
+++ src/main.js
@@ -1,18 +1,70 @@
-
+let newArray = [];
 const getFetchJSON = (url) => {
     fetch(url)
       .then(response => {
         return response.json();
       })
       .then(data => {
-        console.log(data);  
+        newArray.push(data);
+        arrData(newArray); 
       })
       .catch(error => {
         return error;
       });
   };
 
-  arr = ['harry_potter', 'batman', 'superman','it'].forEach((ele) =>{
+   arr = ['roma','Avengers: Infinity War', 'Annihilation', 'The Hangover','John Wick','it'].forEach((ele) =>{
     getFetchJSON(`http://www.omdbapi.com/?t=${ele}&apikey=f3f0406a`);
-  })
+  });
+
+
+ // getFetchJSON(`http://www.omdbapi.com/?t=Avengers: Infinity War&apikey=f3f0406a`);
+
+  const Adventure = document.getElementById('Adventure');
+  const Horror = document.getElementById('Horror');
+  const SciFi = document.getElementById('Sci-Fi');
+  const Drama = document.getElementById('Drama');
+  const Comedy = document.getElementById('Comedy');
+  const Action = document.getElementById('Action');
  
+
+ 
+  const paintingData = (data, id, genres ) => {
+    let dataToHtml = [];
+    data.forEach((ele) => {
+      console.log(ele.Poster);
+       let listData = ` 
+       <div class="card-body">
+       <img src="${ele.Poster}" class="card-img " alt="imgen de ${ele.Title}" >
+            <a href="#" class="btn btn-color"> ${genres} </a>
+          </div>    
+     `;
+      dataToHtml.push(listData); 
+    });
+    id.innerHTML = dataToHtml.join(' ');
+  };
+
+
+  
+  const arrData = (data) => {      
+    const genresAdventure = 'Adventure'
+    const filterAdventure = data.filter(ele => ele.Title.indexOf('Avengers: Infinity War')!==-1);
+       paintingData(filterAdventure, Adventure,genresAdventure);
+       const genresHorror = 'Horror'
+    const filterHorror = data.filter(ele => ele.Title.indexOf('It')!==-1);
+       paintingData(filterHorror, Horror, genresHorror);
+       const genresSciFi = 'Sci-Fi';
+       const filterSciFi = data.filter(ele => ele.Title.indexOf('Annihilation')!==-1);      
+       paintingData(filterSciFi, SciFi, genresSciFi);
+       const genresDrama = 'Drama';
+       const filterDrama = data.filter(ele => ele.Title.indexOf('Roma')!==-1);      
+       paintingData(filterDrama, Drama, genresDrama);
+       const genresComedy = 'Comedy';
+       const filterComedy = data.filter(ele => ele.Title.indexOf('The Hangover')!==-1);      
+       paintingData(filterComedy, Comedy, genresComedy);
+       const genresAction = 'Action';
+       const filterAction = data.filter(ele => ele.Title.indexOf('John Wick')!==-1);      
+       paintingData(filterAction, Action, genresAction);
+  };
+ 
+  
